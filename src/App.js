04323caf9207c5208fa229b0f3a5f8524037bf80
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2020-05-13 16:15:33 -0500

    restructured html in app

--- src/App.js
+++ src/App.js
@@ -21,65 +21,70 @@ class App extends React.Component {
   }
 
   searchFilter = (name) => {
-    this.setState({employees:employees.filter(person => {
-      return person.name.first.toLowerCase().includes(name.toLowerCase())
-      || person.name.last.toLowerCase().includes(name.toLowerCase())
-      || person.phone.replace(/\D/g,"").includes(name)
-      || person.phone.includes(name)
-    })})
+    this.setState({
+      employees: employees.filter(person => {
+        return person.name.first.toLowerCase().includes(name.toLowerCase())
+          || person.name.last.toLowerCase().includes(name.toLowerCase())
+          || person.phone.replace(/\D/g, "").includes(name)
+          || person.phone.includes(name)
+      })
+    })
   }
 
 
   render() {
     return (
-      <div className="App container">
-        <div>
-          <input type="text" placeholder="Search" onChange={(e) => this.searchFilter(e.target.value)}/>
-          <div className="header row flex h4 my-0 p-0 py-2">
-            <div className="col">
-              <div className="d-inline">
-                First
+      <div className="App">
+        <div className="header">
+          <div className="container pt-3">
+            <input type="text" placeholder="Search" onChange={(e) => this.searchFilter(e.target.value)} />
+            <div className="header row flex h4 my-0 p-0 py-2">
+              <div className="col">
+                <div className="d-inline">
+                  First
               </div>
-              <i className={this.state.first > 0 ? "fas fa-sort-amount-down-alt" : "fas fa-sort-amount-up-alt"}
-                onClick={() => { this.handleNameSort("first", this.state.first); this.setState({ first: -this.state.first }) }}></i>
-            </div>
+                <i className={this.state.first > 0 ? "fas fa-sort-amount-down-alt" : "fas fa-sort-amount-up-alt"}
+                  onClick={() => { this.handleNameSort("first", this.state.first); this.setState({ first: -this.state.first }) }}></i>
+              </div>
 
-            <div className="col d-flex">
-              <div>
-                Last
+              <div className="col d-flex">
+                <div>
+                  Last
               </div>
-              <i className={this.state.last > 0 ? "fas fa-sort-amount-down-alt" : "fas fa-sort-amount-up-alt"}
-                onClick={() => { this.handleNameSort("last", this.state.last); this.setState({ last: -this.state.last }) }}></i>
-            </div>
+                <i className={this.state.last > 0 ? "fas fa-sort-amount-down-alt" : "fas fa-sort-amount-up-alt"}
+                  onClick={() => { this.handleNameSort("last", this.state.last); this.setState({ last: -this.state.last }) }}></i>
+              </div>
 
-            <div className="col d-flex">
-              <div>
-                DOB
+              <div className="col d-flex">
+                <div>
+                  DOB
               </div>
-            </div>
+              </div>
 
-            <div className="col d-flex">
-              <div>
-                Gender
+              <div className="col d-flex">
+                <div>
+                  Gender
               </div>
-            </div>
+              </div>
 
-            <div className="col d-flex">
-              <div>
-                Phone
+              <div className="col d-flex">
+                <div>
+                  Phone
               </div>
-            </div>
+              </div>
 
-            <div className="col d-flex">
-              <div>
-                Email
+              <div className="col d-flex">
+                <div>
+                  Email
               </div>
-              <i className={this.state.email > 0 ? "fas fa-sort-amount-down-alt" : "fas fa-sort-amount-up-alt"}
-                onClick={() => { this.handleSort("email", this.state.email); this.setState({ email: -this.state.email }) }}></i>
+                <i className={this.state.email > 0 ? "fas fa-sort-amount-down-alt" : "fas fa-sort-amount-up-alt"}
+                  onClick={() => { this.handleSort("email", this.state.email); this.setState({ email: -this.state.email }) }}></i>
+              </div>
             </div>
           </div>
         </div>
-        <div className="flex-wrap">
+
+        <div className="container flex-wrap">
           {
             this.state.employees.map((employee) => {
               //console.log(employee)
@@ -91,6 +96,7 @@ class App extends React.Component {
             })
           }
         </div>
+        
       </div >
     );
   }
