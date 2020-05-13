a613af531c511207c065b3af78917f7dcef520e9
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2020-05-13 14:57:04 -0500

    Added phone numbers to search

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ class App extends React.Component {
     this.setState({employees:employees.filter(person => {
       return person.name.first.toLowerCase().includes(name.toLowerCase())
       || person.name.last.toLowerCase().includes(name.toLowerCase())
+      || person.phone.includes(name)
     })})
   }
 
