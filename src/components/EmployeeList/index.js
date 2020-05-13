e18198fa3e79179ccfdafe04d14ba76257e34715
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2020-05-13 15:19:15 -0500

    Set up basic cards

--- src/components/EmployeeList/index.js
+++ src/components/EmployeeList/index.js
@@ -3,7 +3,8 @@ import React from "react";
 function EmployeeList(props) {
   console.log(props);
   return (
-    <div className="employee-row row d-flex flex-column flex-md-row py-2" key={props.person.id.value}>
+    <div className="personCard card mt-5" key={props.person.id.value}>
+      <div><img className="personThumb" src={props.person.picture.medium} alt={props.person.name.first + props.person.name.last}/></div>
       <div className="col"><span className="d-inline d-md-none smlabel">First Name:</span>{props.person.name.first}</div>
       <div className="col"><span className="d-inline d-md-none smlabel">Last Name:</span>{props.person.name.last}</div>
       <div className="col"><span className="d-inline d-md-none smlabel">DOB:</span>{props.person.dob.date.split("T")[0]}</div>
