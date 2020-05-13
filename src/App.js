984df489b07368cbc0d73b758c741c7de1acd194
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2020-05-12 23:45:07 -0500

    Added Sorting - first name - last name - email

--- src/App.js
+++ src/App.js
@@ -6,8 +6,20 @@ import EmployeeList from "./components/EmployeeList";
 
 class App extends React.Component {
   state = {
-    employees
+    employees,
+    first: 1,
+    last: 1,
+    email: 1
   }
+
+  handleNameSort = (name, order) => {
+    this.setState({ employees: employees.sort((a, b) => (a.name[name] > b.name[name] ? order : -order)) });
+  }
+
+  handleSort = (name, order) => {
+    this.setState({ employees: employees.sort((a, b) => (a[name] > b[name] ? order : -order)) });
+  }
+
 
   render() {
     return (
@@ -18,50 +30,42 @@ class App extends React.Component {
               <div className="d-inline">
                 First
               </div>
-              <div className=" d-inline text-right">
-                I
-              </div>
+              <i className={this.state.first > 0 ? "fas fa-sort-amount-down-alt" : "fas fa-sort-amount-up-alt"}
+                onClick={() => { this.handleNameSort("first", this.state.first); this.setState({ first: -this.state.first }) }}></i>
             </div>
-            
+
             <div className="col d-flex">
               <div>
                 Last
               </div>
-              <div>
-
-              </div>
+              <i className={this.state.last > 0 ? "fas fa-sort-amount-down-alt" : "fas fa-sort-amount-up-alt"}
+                onClick={() => { this.handleNameSort("last", this.state.last); this.setState({ last: -this.state.last }) }}></i>
             </div>
+
             <div className="col d-flex">
               <div>
                 DOB
               </div>
-              <div>
-
-              </div>
             </div>
+
             <div className="col d-flex">
               <div>
                 Gender
               </div>
-              <div>
-
-              </div>
             </div>
+
             <div className="col d-flex">
               <div>
                 Phone
               </div>
-              <div>
-
-              </div>
             </div>
+
             <div className="col d-flex">
               <div>
                 Email
               </div>
-              <div>
-
-              </div>
+              <i className={this.state.email > 0 ? "fas fa-sort-amount-down-alt" : "fas fa-sort-amount-up-alt"}
+                onClick={() => { this.handleSort("email", this.state.email); this.setState({ email: -this.state.email }) }}></i>
             </div>
           </div>
         </div>
--- src/components/EmployeeList/index.js
+++ src/components/EmployeeList/index.js
@@ -3,13 +3,13 @@ import React from "react";
 function EmployeeList(props) {
   console.log(props);
   return (
-    <div className="employee-row row d-flex flex-column flex-md-row py-2">
-      <div className="col bg-primary"><span className="d-inline d-md-none smlabel">First Name:</span>{props.person.name.first}</div>
-      <div className="col bg-secondary"><span className="d-inline d-md-none smlabel">Last Name:</span>{props.person.name.last}</div>
-      <div className="col bg-secondary"><span className="d-inline d-md-none smlabel">DOB:</span>{props.person.dob.date.split("T")[0]}</div>
-      <div className="col bg-primary"><span className="d-inline d-md-none smlabel">Gender:</span>{props.person.gender}</div>
-      <div className="col bg-secondary"><span className="d-inline d-md-none smlabel">Phone:</span>{props.person.phone}</div>
-      <div className="col bg-primary"><span className="d-inline d-md-none smlabel">Email:</span>{props.person.email}</div>
+    <div className="employee-row row d-flex flex-column flex-md-row py-2" key={props.person.id.value}>
+      <div className="col"><span className="d-inline d-md-none smlabel">First Name:</span>{props.person.name.first}</div>
+      <div className="col"><span className="d-inline d-md-none smlabel">Last Name:</span>{props.person.name.last}</div>
+      <div className="col"><span className="d-inline d-md-none smlabel">DOB:</span>{props.person.dob.date.split("T")[0]}</div>
+      <div className="col"><span className="d-inline d-md-none smlabel">Gender:</span>{props.person.gender}</div>
+      <div className="col"><span className="d-inline d-md-none smlabel">Phone:</span>{props.person.phone}</div>
+      <div className="col"><span className="d-inline d-md-none smlabel">Email:</span>{props.person.email}</div>
     </div>
   )
 }
